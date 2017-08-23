2b777b73633b04aedf02eabf2f32b45a2d3a6387
tcunnington <taylor.cunnington@gmail.com>
2017-08-23 15:35:06 -0700

    more import friendly plot polar angle

--- reobase_analysis/plot_polar_angle.py
+++ reobase_analysis/plot_polar_angle.py
@@ -5,17 +5,10 @@ import numpy as np
 import pandas as pd
 from reobase_analysis.analysis import StimType
 
-SHOW = True
+SAVE = False
 
-cell_gid = [313862022, 314900022, 320668879][1]
-tdc = r.read_cell_tables(cell_gid, [str(x) for x in range(10,21,10)], stim_type=StimType.DC)
-tdc['theta'], tdc['phi'] = ra.spherical_coords(tdc)
-#tdcp = r.read_cell_tables(cell_gid, [str(x) for x in range(20,101,20)], stim_type=StimType.DC_LGN_POISSON)
-#tdcp['theta'], tdcp['phi'] = ra.spherical_coords(tdcp)
 
-#%%
-
-def plot_spikes_polar_angle(t, amp, col_name='num_spikes', nbins=14):
+def plot(t, amp, col_name='num_spikes', nbins=14, save_name=None):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.set_title('"{}" distribution by polar angle $\Theta$ (amp = {})'.format(col_name, amp))
@@ -38,18 +31,36 @@ def plot_spikes_polar_angle(t, amp, col_name='num_spikes', nbins=14):
     ax.boxplot(data, showmeans=True)
     ax.set_xticklabels(["{0:.2f}".format(x) for x in np.array(rads)/np.pi])
     
-    if SHOW:
-        plt.show()
+    if save_name is not None:
+        plt.savefig(save_name)
     else:
-        plt.savefig('polar_angle_spikes_{}_'.format(cell_gid) + r.format_amp(amp) + '.png')
+        plt.show()
+
+
+def fetch_data_plot(cell_gid, amp, stim_type, *args, **kwargs):
+    """ Same as 'plot' but fetches data for you """
+    t = r.read_cell_tables(cell_gid, [amp], stim_type=stim_type)
+    t['theta'], t['phi'] = ra.spherical_coords(t)
+    plot(t, amp, *args, **kwargs)
 
 #%%
-        
-data = tdc
-data = data[(data.distance == 35)] # exclude closest layer
-#df = pd.DataFrame(index=data.electrode)
 
-for amp, g in data.groupby('amp'):
-#    print amp, g['num_spikes']
-#    df[amp] = g['num_spikes']
-    plot_spikes_polar_angle(g, amp, col_name='delta_vm')
+def default():
+    cell_gid = [313862022, 314900022, 320668879][1]
+    t = r.read_cell_tables(cell_gid, [str(x) for x in range(10, 21, 10)], stim_type=StimType.DC)
+    t['theta'], t['phi'] = ra.spherical_coords(t)
+
+    # tdcp = r.read_cell_tables(cell_gid, [str(x) for x in range(20,101,20)], stim_type=StimType.DC_LGN_POISSON)
+    # tdcp['theta'], tdcp['phi'] = ra.spherical_coords(tdcp)
+
+    data = t
+    data = data[(data.distance == 35)] # exclude closest layer
+
+
+    for amp, g in data.groupby('amp'):
+        # save_name = 'polar_angle_spikes_{}_'.format(cell_gid) + r.format_amp(amp) + '.png'
+        plot(g, amp, col_name='delta_vm')
+
+
+
+# default()
